ca258a1780f452f3d3cd02523f5d5bb53f7acc13
sheepdog13 <mikecho1234@naver.com>
2023-11-20 23:02:41 +0900

    jotai로 Map 객체 전역관리, 맵 클릭 이벤트 추가

--- mern_client/src/components/MapContainer.tsx
+++ mern_client/src/components/MapContainer.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Map from "./common/Map";
+import { mapAtom } from "../atoms/map";
+import { useSetAtom } from "jotai";
 
 function MapContainer() {
-  return <Map width="100%" height="100%" />;
+  const setMap = useSetAtom(mapAtom);
+
+  const ininMap = (map: naver.maps.Map) => {
+    setMap(map);
+    naver.maps.Event.addListener(map, "click", () => {
+      console.log("맵 클릭");
+    });
+  };
+
+  return <Map width="100%" height="100%" ininMap={ininMap} />;
 }
 
 export default MapContainer;
--- mern_client/src/components/common/Map/index.tsx
+++ mern_client/src/components/common/Map/index.tsx
@@ -3,9 +3,10 @@ import React, { useEffect } from "react";
 interface MapProps {
   width: string;
   height: string;
+  ininMap?: (map: naver.maps.Map) => void;
 }
 
-function Map({ width, height }: MapProps) {
+function Map({ width, height, ininMap }: MapProps) {
   useEffect(() => {
     const mapOptions = {
       center: new naver.maps.LatLng(37.3595704, 127.105399),
@@ -13,6 +14,9 @@ function Map({ width, height }: MapProps) {
     };
 
     const map = new naver.maps.Map("map", mapOptions);
+    if (ininMap) {
+      ininMap(map);
+    }
   }, []);
   return <div id="map" style={{ width, height }}></div>;
 }
